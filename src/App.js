45b4b405e3799a3b9a9af8932e451a3eeca29736
sangbin <sangbin@bluega.com>
2020-03-18 18:44:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+var gTR;
+
+function init() {
+  window.addEventListener(
+    "message",
+    function(ev) {
+      if (ev.data && ev.data.mqfEditor) {
+        var event = ev.data.mqfEditor.event;
+        var data = ev.data.mqfEditor.data;
+
+        if (event == "edit") {
+          // 편집할 Asset
+          document.getElementById("assetId").value = data.value;
+
+          gTR = data;
+
+          search(data.type, data.value);
+        } else if (event == "reset") {
+          // 화면 Reset
+          gTR = undefined;
+
+          document.getElementById("assetId").value = "";
+          document.getElementById("preview").innerHTML = "";
+        }
+      }
+    },
+    false
+  );
+}
+
+//  Asset을 Search
+function search(type, id) {
+  if (type == "image") type = "I";
+  else if (type == "audio") type = "A";
+  else if (type == "video") type = "V";
+
+  var url = "http://aspenux.com/tool/ecc/asset/" + type + "/" + id;
+
+  if (type == "I") {
+    document.getElementById("preview").innerHTML =
+      '<img style="max-width:100%;" src="' + url + '"/>';
+  } else if (type == "A") {
+    document.getElementById("preview").innerHTML =
+      '<audio controls style="max-width:100%;" src="' + url + '"/>';
+  } else if (type == "V") {
+    document.getElementById("preview").innerHTML =
+      '<video controls style="max-width:100%;" src="' + url + '"/>';
+  }
+}
+
+const onClickSearch = () => {
+  if (gTR) search(gTR.type, document.getElementById("assetId").value);
+};
+
+//  결과를 Editor로 전송함. %%INFO Asset이 존재하면, 항상 JSON의 구성상으로는 Madatory이므로 값이 없으면 전송하지 않음
+function set() {
+  var value = document.getElementById("assetId").value;
+
+  if (!(gTR && value)) return;
+
+  window.parent.postMessage(
+    {
+      mqfEditor: {
+        event: "onSetMeta",
+        data: {
+          target: gTR.target,
+          trId: gTR.trId,
+          type: gTR.type,
+          value: value
+        }
+      }
+    },
+    "*"
+  );
+}
 
 function App() {
+  init();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      PANDO - ASSET MANAGER
+      <br />
+      <br />
+      ID <input id="assetId" type="text"></input>{" "}
+      <span onClick={onClickSearch}>[Search]</span>{" "}
+      <span onClick={set}>[Set]</span>
+      <br />
+      <div id="preview"></div>
+    </>
   );
 }
 
